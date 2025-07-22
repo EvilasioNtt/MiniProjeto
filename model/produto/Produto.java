5a708e2864eec52b1ba0c10114528de49cf6f3bc
LucasRoudrigues <rodrigueslucas9889@gmail.com>
2025-07-21 21:22:05 -0300

    Update Produto.java

---
+++ model/produto/Produto.java
@@ -0,0 +1,38 @@
+package loja.model.produto;
+
+public abstract class Produto {
+    private String codigo; 
+    private String nome;
+    private double precoBase; 
+    public Produto(String codigo, String nome, double precoBase) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.precoBase = precoBase;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        if (precoBase < 0) {
+            precoBase = 0.0;
+        }
+        this.precoBase = precoBase;
+    }
+
+    public double getPrecoVenda() {
+        return precoBase;
+    }
